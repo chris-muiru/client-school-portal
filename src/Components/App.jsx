2d0fba17581b61f0556c7fc4b555c060cac04ca7
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-03-05 20:13:38 +0300

    add: App component

---
+++ src/Components/App.jsx
@@ -0,0 +1,26 @@
+import VerificationPage from './LogIn/VerificationPage'
+import { BrowserRouter, Route} from 'react-router-dom';
+import Main from './Main';
+import AuthProvider from '../Context/AuthContext';
+
+import PrivateRoute from '../utils/PrivateRoute';
+
+const App=()=>{
+    return(
+        <div>
+            <AuthProvider>
+                <BrowserRouter>
+                    <PrivateRoute path="/dash">
+                        <Main/>
+                    </PrivateRoute>
+                    <Route path="/login">
+                        <VerificationPage/>
+                    </Route>
+                </BrowserRouter>
+            </AuthProvider>
+                        
+        </div>
+    )
+}
+
+export default App;
