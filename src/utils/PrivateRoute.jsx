bdcd4eafe9f046b988d8687929a37bd94f97fb66
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-03-05 20:14:12 +0300

    add: add private router

---
+++ src/utils/PrivateRoute.jsx
@@ -0,0 +1,14 @@
+import { Route,Redirect } from "react-router-dom";
+import { useAuthContext } from "../Context/AuthContext";
+const PrivateRoute=({children,...rest})=>{
+    let {user} = useAuthContext();
+    return(
+        <Route {...rest}>
+            {!user ? <Redirect to="/login"/> : children}
+        </Route>
+    ) 
+
+}
+
+
+export default PrivateRoute;
