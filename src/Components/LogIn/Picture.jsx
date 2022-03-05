0d87865b4a254523c4c1dcb75e7cef0aa7f20881
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-03-05 20:29:20 +0300

    add: login picture component

---
+++ src/Components/LogIn/Picture.jsx
@@ -0,0 +1,11 @@
+import study from  '../img/LogIn/study2.jpg';
+import '../css/LogIn/picture.css'
+const Picture=()=>{
+    return(
+        <div className="div-picture">
+            <img src={study} alt="pencil" className="picture"/>
+
+        </div>
+    )
+}
+export default Picture;
