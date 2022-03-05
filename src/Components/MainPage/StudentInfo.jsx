dccbb58ac7314725852509416173b8236cfa50f6
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-03-05 20:20:33 +0300

    add: StudentInfo component that fetches user data

---
+++ src/Components/MainPage/StudentInfo.jsx
@@ -0,0 +1,70 @@
+import '../css/MainPage/studentinfo.css'
+import {useAuthContext} from '../../Context/AuthContext'
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+
+const StudentInfo=()=>{   
+    const [studentData,setStudentData]=useState({});
+    let {authTokens}=useAuthContext();
+
+
+    let fetchUserInfo=async()=>{
+        const USER_INFO_URL='http://localhost:8000/student-detail/'
+       
+        let response=await fetch(USER_INFO_URL,{
+            method:'GET',
+            headers:{
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${ authTokens.access}`
+            },
+                // body:JSON.stringif) 
+        })
+        let data=await response.json()
+        setStudentData(data)
+    }
+
+
+    useEffect(()=>{
+        fetchUserInfo()
+    },[])
+    console.log(studentData)
+    return(
+        <div className="div-studentinfo">
+            <h3 className="heading-studentinfo">Student data</h3>     
+            <table>
+                <tbody>
+                    <tr>
+                        <td>school id</td>
+                        <td>{studentData.school_id}</td>
+                    </tr>
+                    <tr>
+                        <td>sex</td>
+                        <td>{studentData.gender}</td>
+                    </tr>  
+                    <tr>
+                        <td>country</td>
+                        <td>{studentData.country}</td>
+                    </tr>
+                    <tr>
+                        <td>city</td>
+                        <td>{studentData.city}</td>
+                    </tr>
+                    <tr>
+                        <td>phone</td>
+                        <td>{studentData.phone}</td>
+                    </tr>
+                    <tr>
+                        <td>Email</td>
+                        <td>{studentData.school_email}</td>
+                    </tr>
+
+
+
+                </tbody>
+            </table>
+        </div>
+
+    )
+}
+export default StudentInfo;
