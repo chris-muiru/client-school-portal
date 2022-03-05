7f6685eddf33cd38a83aadcb1dad44ece5fd4b05
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-03-05 20:32:03 +0300

    add: Trow component to output each row containing a single unit details

---
+++ src/Components/Results/Trow.jsx
@@ -0,0 +1,14 @@
+const Trow=({unit_name,unit_code,marks,grade})=>{
+    return(
+            
+        <tr>
+            <td>{unit_name}</td>
+            <td>{unit_code}</td>
+            <td>{marks}</td>
+            <td>{grade}</td>
+        </tr>
+      
+    )
+}
+
+export default Trow;
