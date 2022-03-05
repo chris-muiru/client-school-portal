d363b9e49b1ca72efb7da4042a138eaa231e2fb6
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-03-05 20:18:46 +0300

    add: add header

---
+++ src/Components/MainPage/Header.jsx
@@ -0,0 +1,12 @@
+import '../css/MainPage/header.css';
+import Menubar from './Menubar';
+const Header=()=>{
+    return(
+        <div className="div-header">
+            <Menubar/>
+            Student portal
+
+        </div>
+    )
+}
+export default Header;
