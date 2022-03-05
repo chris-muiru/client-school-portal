8a75bd5cb8286a4631ea829bb0851344729b0566
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-03-05 20:28:24 +0300

    add: Forgot component

---
+++ src/Components/LogIn/Forgot.jsx
@@ -0,0 +1,10 @@
+import '../css/LogIn/forgot.css';
+const Forgot=()=>{
+    return(
+        <div className="div-forgot">
+            <small>forgot password/username?</small>
+        </div>
+    );
+};
+
+export default Forgot;
