d2fd9b696a7811c841af4195ebf503963905c2ef
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-03-08 09:47:45 +0300

    add: add heding component

--- src/Components/MainPage/Header.jsx
+++ src/Components/MainPage/Header.jsx
@@ -1,12 +1,11 @@
-import '../css/MainPage/header.css';
-import Menubar from './Menubar';
-const Header=()=>{
-    return(
-        <div className="div-header">
-            <Menubar/>
-            Student portal
-
-        </div>
-    )
-}
+import "../css/MainPage/header.css";
+import Menubar from "./Menubar";
+const Header = () => {
+	return (
+		<div className="div-header">
+			<Menubar />
+			Student portal
+		</div>
+	);
+};
 export default Header;
