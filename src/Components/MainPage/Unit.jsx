2f220d2e772f96ca510c462174ed112161c119f3
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-03-08 09:51:28 +0300

    add: add chilren prop

--- src/Components/MainPage/Unit.jsx
+++ src/Components/MainPage/Unit.jsx
@@ -1,16 +1,15 @@
-import '../css/MainPage/unit.css';
-const Unit=({unitDetail})=>{
-    let choice=['orangered','rgb(98, 17, 173)','green','red'];
-    let backgroundColor=choice[Math.floor(Math.random()*choice.length)];
+import "../css/MainPage/unit.css";
+const Unit = ({ unitDetail, children }) => {
+  let choice = ["orangered", "rgb(98, 17, 173)", "green", "red"];
+  let backgroundColor = choice[Math.floor(Math.random() * choice.length)];
 
-    return(
-        <div className="div-unit" style={{backgroundColor}}>
-            {/*from django database*/}            
-            <h4>{unitDetail}</h4>
-
-        </div>
-    )
-
-}
+  return (
+    <div className="div-unit" style={{ backgroundColor }}>
+      {/*from django database*/}
+      <h4>{unitDetail}</h4>
+      <p>{children}</p>
+    </div>
+  );
+};
 
 export default Unit;
