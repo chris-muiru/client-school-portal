250053aa97e3b8a5c8668e850b34f7c3891c98f6
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-03-08 09:48:28 +0300

    add: fetch unit count and output the details

--- src/Components/MainPage/UnitGroup.jsx
+++ src/Components/MainPage/UnitGroup.jsx
@@ -1,25 +1,48 @@
-import  Unit from "./Unit";
-import '../css/MainPage/unitgroup.css';
+import Unit from "./Unit";
+import "../css/MainPage/unitgroup.css";
 import StudentInfo from "./StudentInfo";
 import Fee from "./Fee";
 import StudentPic from "./StudentPic";
 import Header from "./Header";
+import { useAuthContext } from "../../Context/AuthContext";
+import { useEffect, useState } from "react";
 
+const UnitGroup = () => {
+	const [count, setCount] = useState({});
+	const { getAuthToken } = useAuthContext();
+	const fetchUnitCount = async () => {
+		const UNIT_COUNT_URL = "http://localhost:8000/unitsDetail/";
+		let response = await fetch(UNIT_COUNT_URL, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${getAuthToken()}`,
+			},
+		});
 
-const UnitGroup=()=>{
-    return(
-        <div className="div-unitgroup">
-            <Header/>
-            <StudentPic/>
-            <h3 className="h2-units">Units Info</h3>
-            <Unit unitDetail="units done:"/>
-            <Unit unitDetail="selected units:"/>
-            <Unit unitDetail="remaining units:"/>
-            <StudentInfo/>
-            
-            <Fee/>
-        </div>
-    )
-}
+		const data = await response.json();
+		setCount(data);
+	};
+
+	useEffect(() => {
+		fetchUnitCount();
+	}, []);
+
+	const { total_units, booked_units, remaining_units } = count;
+
+	return (
+		<div className="div-unitgroup">
+			<Header />
+			<StudentPic />
+			<h3 className="h2-units">Units Info</h3>
+			<Unit unitDetail="units done:">{total_units}</Unit>
+			<Unit unitDetail="selected units:">{booked_units}</Unit>
+			<Unit unitDetail="remaining units:">{remaining_units}</Unit>
+			<StudentInfo />
+
+			<Fee />
+		</div>
+	);
+};
 
 export default UnitGroup;
