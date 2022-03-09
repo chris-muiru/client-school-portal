cf23ab71c9dfec3d6f6a1d9a596fff8278b1bc16
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-03-09 22:08:59 +0300

    add: add logout in heder

--- src/Components/MainPage/Header.jsx
+++ src/Components/MainPage/Header.jsx
@@ -1,10 +1,13 @@
 import "../css/MainPage/header.css";
+import LogOut from "../LogOut/LogOut";
+
 import Menubar from "./Menubar";
 const Header = () => {
 	return (
 		<div className="div-header">
 			<Menubar />
 			Student portal
+			<LogOut />
 		</div>
 	);
 };
