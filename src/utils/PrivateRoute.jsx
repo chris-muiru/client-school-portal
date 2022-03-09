a20262ace72dd561d4454c5144e104b867a29e3a
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-03-09 22:18:05 +0300

    style: using prettier

--- src/utils/PrivateRoute.jsx
+++ src/utils/PrivateRoute.jsx
@@ -1,14 +1,10 @@
-import { Route,Redirect } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 import { useAuthContext } from "../Context/AuthContext";
-const PrivateRoute=({children,...rest})=>{
-    let {user} = useAuthContext();
-    return(
-        <Route {...rest}>
-            {!user ? <Redirect to="/login"/> : children}
-        </Route>
-    ) 
-
-}
-
+const PrivateRoute = ({ children, ...rest }) => {
+	let { user } = useAuthContext();
+	return (
+		<Route {...rest}>{!user ? <Redirect to="/login" /> : children}</Route>
+	);
+};
 
 export default PrivateRoute;
