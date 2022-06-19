e3e0773a1f09672501fe22902fe81a9c56edd701
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-06-19 17:30:24 +0300

    refactor: refactor view

--- src/Components/LogIn/Login.jsx
+++ src/Components/LogIn/Login.jsx
@@ -6,14 +6,13 @@ const Login=()=>{
     return(
         <div className="div-login">
             <h2 className="heading">Log in</h2>
-            <div className="login">
-                <form onSubmit={loginUser}>
-                    <input type="text" placeholder="username" name="username" className="input" />  
-                    <input type="password" placeholder="password" name="password"  className="input" />    
+                <form onSubmit={loginUser} className="login">
+                    <input type="text" placeholder="Username" name="username" className="input" /> 
+                    <input type="password" placeholder="Password" name="password"  className="input" />    
                     <button type="submit" id="btn-submit">Log in</button>
-                    <Forgot/>
-                </form> 
-            </div> 
+                    {/* <Forgot/> */}
+                </form>
+                <Forgot/> 
         </div>
     )
 }
