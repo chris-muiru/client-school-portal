06c57d44f10daaf64ed208311005457d77743d16
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-06-20 10:55:42 +0300

    update: auth context,get token if not expired

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -1,10 +1,10 @@
-import { createContext, useContext, useEffect, useState } from "react";
-import jwtDecode from "jwt-decode";
-const AuthContext = createContext();
+import { createContext, useContext, useEffect, useState } from "react"
+import jwtDecode from "jwt-decode"
+const AuthContext = createContext()
 
 export function useAuthContext() {
 	// custom hook
-	return useContext(AuthContext);
+	return useContext(AuthContext)
 }
 
 const AuthProvider = ({ children }) => {
@@ -12,16 +12,16 @@ const AuthProvider = ({ children }) => {
 		localStorage.getItem("authTokens")
 			? jwtDecode(localStorage.getItem("authTokens"))
 			: null
-	);
+	)
 	const [authTokens, setAuthTokens] = useState(() =>
 		localStorage.getItem("authTokens")
 			? JSON.parse(localStorage.getItem("authTokens"))
 			: null
-	);
+	)
 
 	let loginUser = async (e) => {
-		const GET_TOKEN = "http://localhost:8000/api/token/";
-		e.preventDefault();
+		const GET_TOKEN = "http://localhost:8000/api/token/"
+		e.preventDefault()
 		let response = await fetch(GET_TOKEN, {
 			method: "POST",
 			headers: {
@@ -31,82 +31,86 @@ const AuthProvider = ({ children }) => {
 				username: e.target.username.value,
 				password: e.target.password.value,
 			}),
-		});
-		let data = await response.json();
-		console.log(` ----- ${data} is the data`);
+		})
+		let data = await response.json()
+		console.log(` ----- ${data} is the data`)
 		// jwt decode to decode user data
 		if (response.status === 200) {
-			setAuthTokens(data);
-			setUser(jwtDecode(data.access));
-			localStorage.setItem("authTokens", JSON.stringify(data));
+			setAuthTokens(data)
+			setUser(jwtDecode(data.access))
+			localStorage.setItem("authTokens", JSON.stringify(data))
 
-			console.log(`1. ${authTokens}`);
+			console.log(`1. ${authTokens}`)
 
-			window.location = "/dash";
+			window.location = "/dash"
 		} else {
-			alert("something went wrong");
+			alert("something went wrong")
 		}
-	};
+	}
 
 	let logOutUser = () => {
-		setAuthTokens(null);
-		setUser(null);
-		localStorage.removeItem("authTokens");
+		setAuthTokens(null)
+		setUser(null)
+		localStorage.removeItem("authTokens")
 
-		window.location = "/login";
-	};
+		window.location = "/login"
+	}
 
 	let updateTokens = async () => {
-		const REFRESH_TOKEN = "http://localhost:8000/api/token/refresh/";
-		console.log(authTokens.refresh);
+		const REFRESH_TOKEN = "http://localhost:8000/api/token/refresh/"
+		console.log(authTokens.refresh)
 		let response = await fetch(REFRESH_TOKEN, {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json",
 			},
 			body: JSON.stringify({ refresh: authTokens.refresh }),
-		});
-		let data = await response.json();
+		})
+		let data = await response.json()
 
 		if (response.status === 200) {
-			setAuthTokens(data);
-			setUser(jwtDecode(data.access));
-			localStorage.setItem("authTokens", JSON.stringify(data));
+			setAuthTokens(data)
+			setUser(jwtDecode(data.access))
+			localStorage.setItem("authTokens", JSON.stringify(data))
 		} else {
-			logOutUser();
-			window.location = "login";
+			logOutUser()
+			window.location = "login"
 		}
-	};
+	}
 
 	const getAuthToken = () => {
-		return authTokens.access;
-	};
+		if (jwtDecode(authTokens.access).exp < Date.now() / 1000) {
+			localStorage.clear()
+			window.location = "/auth"
+		}
+		return authTokens.access
+	}
 
 	let contextData = {
 		user: user,
 		loginUser: loginUser,
 		logOutUser: logOutUser,
 		getAuthToken: getAuthToken,
-	};
+	}
 
 	useEffect(() => {
-		console.log("update token called");
+		console.log("update token called")
 		let interval = setInterval(() => {
 			if (authTokens) {
-				updateTokens();
+				updateTokens()
 			}
-		}, 240000);
+		}, 240000)
 
 		// cleanup
 		return () => {
-			clearInterval(interval);
-		};
-	}, [authTokens]);
+			clearInterval(interval)
+		}
+	}, [authTokens])
 	return (
 		<AuthContext.Provider value={contextData}>
 			{children}
 		</AuthContext.Provider>
-	);
-};
+	)
+}
 
-export default AuthProvider;
+export default AuthProvider
