8dd4560ae75413e4eb30e69927ef7ee302d08f14
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-06-27 18:21:27 +0300

    style: style using prettier and remove all logs

--- src/Components/Results/StudentResult.jsx
+++ src/Components/Results/StudentResult.jsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
-import "../css/Results/studentresult.css";
-import { useEffect } from "react";
-import { useAuthContext } from "../../Context/AuthContext";
+import { useState } from "react"
+import "../css/Results/studentresult.css"
+import { useEffect } from "react"
+import { useAuthContext } from "../../Context/AuthContext"
 
-import Trow from "./Trow";
+import Trow from "./Trow"
 const StudentResult = () => {
-	const [result, setResult] = useState({});
+	const [result, setResult] = useState({})
 
-	const { getAuthToken } = useAuthContext();
+	const { getAuthToken } = useAuthContext()
 
-	const RESULT_URL = "http://localhost:8000/results/";
+	const RESULT_URL = "http://localhost:8000/results/"
 	const getResult = async () => {
 		let response = await fetch(RESULT_URL, {
 			method: "GET",
@@ -17,18 +17,16 @@ const StudentResult = () => {
 				"Content-Type": "application/json",
 				Authorization: `Bearer ${getAuthToken()}`,
 			},
-		});
-		let data = await response.json();
-		console.log(data);
-		setResult(data);
-	};
+		})
+		let data = await response.json()
+		setResult(data)
+	}
 	useEffect(() => {
-		getResult();
-	}, []);
+		getResult()
+	}, [])
 
-	console.log(result);
 	// set the rows here
-	let rows = [];
+	let rows = []
 	for (let i = 0; i < result.length; i++) {
 		rows.push(
 			<Trow
@@ -36,15 +34,13 @@ const StudentResult = () => {
 				unit_code={result[i]["unit_code"]}
 				marks={result[i]["marks"]}
 				grade={result[i]["grade"]}
-				year={result[i]["year"]}
 			/>
-		);
+		)
 	}
 	return (
 		<div>
 			<div className="div-result">
-				<div className="semister">
-					<h3 className="h3-heading">Semister: </h3>
+					<h3 className="semister-heading">Semister: </h3>
 					<table>
 						<thead>
 							<tr>
@@ -64,9 +60,9 @@ const StudentResult = () => {
 						</thead>
 						<tbody>{rows}</tbody>
 					</table>
+					<h3 className="score-header">Cumulative score:</h3>
 				</div>
-			</div>
 		</div>
-	);
-};
-export default StudentResult;
+	)
+}
+export default StudentResult
