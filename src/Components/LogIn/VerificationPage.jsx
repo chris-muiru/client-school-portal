dcfccf2c8b621cdf55cc213c8bf6f884030585ab
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-06-27 18:18:43 +0300

    style: style using prettier

--- src/Components/LogIn/VerificationPage.jsx
+++ src/Components/LogIn/VerificationPage.jsx
@@ -1,18 +1,21 @@
-import Login from "./Login";
-import Picture from "./Picture";
-import Heading from "./Heading";
-import '../css/LogIn/verificationpage.css';
-import AuthProvider from "../../Context/AuthContext";
-const VerificationPage=()=>{
-    return(
-        <div className="verification">
-            <Heading/>
-            <Picture/>
-            <AuthProvider>
-                <Login/>
-            </AuthProvider>
-        </div>
-    )
+import Login from "./Login"
+import Picture from "./Picture"
+import Heading from "./Heading"
+import "../css/LogIn/verificationpage.css"
+import AuthProvider from "../../Context/AuthContext"
+import Forgot from "./Forgot"
+const VerificationPage = () => {
+	return (
+		<div className="verification">
+			<div className="wrapper-verfication">
+				<Heading />
+				<Picture />
+				<AuthProvider>
+					<Login />
+				</AuthProvider>
+			</div>
+		</div>
+	)
 }
 
-export default VerificationPage;
+export default VerificationPage
