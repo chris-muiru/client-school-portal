6c6e071cafa4e2d7874e46fe7b7ae8a028b12447
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-06-27 18:17:26 +0300

    style: style using prettier

--- src/Components/LogIn/Login.jsx
+++ src/Components/LogIn/Login.jsx
@@ -1,22 +1,31 @@
-import { useAuthContext } from '../../Context/AuthContext';
-import '../css/LogIn/login.css'
-import Forgot from "./Forgot";
-const Login=()=>{
-    let {loginUser} = useAuthContext()
-    return(
-        <div className="div-login">
-            <h2 className="heading">Log in</h2>
-                <form onSubmit={loginUser} className="login">
-                    <input type="text" placeholder="Username" name="username" className="input" /> 
-                    <input type="password" placeholder="Password" name="password"  className="input" />    
-                    <button type="submit" id="btn-submit">Log in</button>
-                    {/* <Forgot/> */}
-                </form>
-                <Forgot/> 
-        </div>
-    )
+import { useAuthContext } from "../../Context/AuthContext"
+import "../css/LogIn/login.css"
+import Forgot from "./Forgot"
+const Login = () => {
+	let { loginUser } = useAuthContext()
+	return (
+		<div className="div-login">
+			<h2 className="heading">Log in</h2>
+			<form onSubmit={loginUser} className="login">
+				<input
+					type="text"
+					placeholder="Username"
+					name="username"
+					className="input"
+				/>
+				<input
+					type="password"
+					placeholder="Password"
+					name="password"
+					className="input"
+				/>
+				<button type="submit" id="btn-submit">
+					Log in
+				</button>
+			</form>
+			<Forgot />
+		</div>
+	)
 }
 
-export default Login;
-
-
+export default Login
